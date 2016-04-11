16a84e823ad427f1e14fde193d41f2d76d4a8795
apedic <antoniopdcn@gmail.com>
2016-04-11 17:37:25 +0100

    commit module for training rewrite sales/order model

---
+++ app/code/local/Apedic/Training/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apedic
+ * Date: 10/04/16
+ * Time: 22:55
+ */ 
+class Apedic_Training_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Apedic/Training/Model/Sales/Order.php
@@ -0,0 +1,10 @@
+<?php
+
+class Apedic_Training_Model_Sales_Order extends Mage_Sales_Model_Order{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
